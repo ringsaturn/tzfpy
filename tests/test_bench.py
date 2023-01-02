cabe40771689e6460035d25ec135a5caeeb83dec
ringsaturn <ringsaturn.me@gmail.com>
2023-01-02 13:32:48 +0800

    add benchmark

--- tests/test_bench.py
+++ tests/test_bench.py
@@ -1,27 +1,13 @@
-# wait until python binding for cities.json
+from citiespy import random_city
+from tzfpy import get_tz
 
-# import json
-# import random
-
-# from tzfpy import get_tz
+_ = random_city()
 
 
-# cities = []
-# with open("tests/cities.json") as f:
-#     raw_cities = json.loads(f.read())
-# for rawcity in raw_cities:
-#     cities.append([float(rawcity["lng"]), float(rawcity["lat"])])
+def _test_tzfpy_random_city():
+    city = random_city()
+    _ = get_tz(city.lng, city.lat)
 
 
-# def random_city():
-#     index = random.randint(0, len(cities)-1)
-#     return cities[index]
-
-# def _test_tzfpy_random_city():
-#     lng, lat = random_city()
-#     _ = get_tz(lng, lat)
-
-
-# def test_tzfpy_random_cities(benchmark):
-#     benchmark(_test_tzfpy_random_city)
-
+def test_tzfpy_random_cities(benchmark):
+    benchmark(_test_tzfpy_random_city)
