c984ab47156e2db92a34613ea2e4a6a471dcb061
Han Xiao <ringsaturn.me@gmail.com>
2025-05-11 10:19:27 +0900

    Update README

--- examples/tzfpy_with_datetime.py
+++ examples/tzfpy_with_datetime.py
@@ -1,12 +1,11 @@
-from datetime import UTC as DT_UTC
-from datetime import datetime
+from datetime import datetime, timezone
 from zoneinfo import ZoneInfo
 
 from tzfpy import get_tz
 
 tz = get_tz(139.7744, 35.6812)  # Tokyo
 
-now = datetime.now(DT_UTC)
+now = datetime.now(timezone.utc)
 now = now.replace(tzinfo=ZoneInfo(tz))
 print(now)
 # 2025-04-29 01:33:56.325194+09:00
