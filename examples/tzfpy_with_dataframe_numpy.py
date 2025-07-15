0d4d7c2c1d2def29bc7e3e7eca46df028cc00f8f
Han Xiao <ringsaturn.me@gmail.com>
2025-07-15 09:59:53 +0900

    add dataframe examples (#95)

---
+++ examples/tzfpy_with_dataframe_numpy.py
@@ -0,0 +1,30 @@
+import time
+
+import citiespy
+import numpy as np
+import pandas as pd
+import tzfpy
+
+# lazy init
+_ = tzfpy.get_tz(0, 0)
+
+
+cities_as_dict = []
+for city in citiespy.all_cities():
+    cities_as_dict.append({"name": city.name, "lng": city.lng, "lat": city.lat})
+
+df = pd.DataFrame(cities_as_dict)
+
+lngs = df.lng.values
+lats = df.lat.values
+
+vec_tzfpy_get_tz = np.vectorize(tzfpy.get_tz)
+
+start = time.perf_counter()
+_ = vec_tzfpy_get_tz(lngs, lats)
+end = time.perf_counter()
+print(f"[tzfpy_with_dataframe] Numpy Time taken: {end - start} seconds")
+
+"""
+[tzfpy_with_dataframe] Numpy Time taken: 0.33512612502090633 seconds
+"""
---
+++ examples/tzfpy_with_dataframe_polars.py
@@ -0,0 +1,30 @@
+import time
+
+import citiespy
+import polars as pl
+import tzfpy
+
+# lazy init
+_ = tzfpy.get_tz(0, 0)
+
+
+cities_as_dict = []
+for city in citiespy.all_cities():
+    cities_as_dict.append({"name": city.name, "lng": city.lng, "lat": city.lat})
+
+df = pl.from_dicts(cities_as_dict)
+
+start = time.perf_counter()
+df = df.with_columns(
+    pl.struct(["lng", "lat"])
+    .map_elements(
+        lambda cols: tzfpy.get_tz(cols["lng"], cols["lat"]), return_dtype=pl.Utf8
+    )
+    .alias("tz_from_tzfpy")
+)
+end = time.perf_counter()
+print(f"[tzfpy_with_dataframe] Polars Time taken: {end - start} seconds")
+
+"""
+[tzfpy_with_dataframe] Polars Time taken: 0.34632241702638566 seconds
+"""
