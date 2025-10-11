421a189c4fab2bedab8ee5cc7f7a630cbcf401ca
Han Xiao <ringsaturn.me@gmail.com>
2025-10-11 13:07:23 +0900

    try auto stub gen

--- src/lib.rs
+++ src/lib.rs
@@ -1,28 +1,31 @@
-// #![allow(unused)]
-
 use lazy_static::lazy_static;
 use pyo3::prelude::*;
 use tzf_rs::DefaultFinder;
+use pyo3_stub_gen::{derive::gen_stub_pyfunction, define_stub_info_gatherer};
 
 lazy_static! {
     static ref FINDER: DefaultFinder = DefaultFinder::default();
 }
 
+#[gen_stub_pyfunction]
 #[pyfunction]
 pub fn get_tz(lng: f64, lat: f64) -> PyResult<String> {
     Ok(FINDER.get_tz_name(lng, lat).to_string())
 }
 
+#[gen_stub_pyfunction]
 #[pyfunction]
 pub fn get_tzs(lng: f64, lat: f64) -> PyResult<Vec<&'static str>> {
     Ok(FINDER.get_tz_names(lng, lat))
 }
 
+#[gen_stub_pyfunction]
 #[pyfunction]
 pub fn timezonenames() -> PyResult<Vec<&'static str>> {
     return Ok(FINDER.timezonenames());
 }
 
+#[gen_stub_pyfunction]
 #[pyfunction]
 pub fn data_version() -> PyResult<String> {
     return Ok(FINDER.data_version().to_string());
@@ -36,3 +39,5 @@ fn tzfpy(_py: Python, m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(data_version, m)?)?;
     Ok(())
 }
+
+define_stub_info_gatherer!(stub_info);
