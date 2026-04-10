37007f889149bae698ca5d3bf3e718c41bfeac9d
Han Xiao <ringsaturn.me@gmail.com>
2026-04-10 20:45:46 +0900

    Stabilize benchmark inputs with seeded random query stream

--- tests/test_bench.py
+++ tests/test_bench.py
@@ -7,28 +7,46 @@ from pytest import mark
 from tzfpy import get_tz
 
 EXP_INDEX = os.getenv("_TZFPY_EXP_INDEX")
-DATASET_SIZE = 4096
 DATASET_SEED = 20260410
+DATASET_SIZE = 8192
 
 _ALL_CITIES = list(all_cities())
 _RNG = random.Random(DATASET_SEED)
-_FIXED_CITIES = _RNG.sample(_ALL_CITIES, min(DATASET_SIZE, len(_ALL_CITIES)))
-_FIXED_POINTS = [(city.lng, city.lat) for city in _FIXED_CITIES]
-
-# warmup lazy init
-_ = get_tz(116.3883, 39.9289)
-for lng, lat in _FIXED_POINTS[:64]:
-    _ = get_tz(lng, lat)
+_RANDOM_POINTS = [
+    (city.lng, city.lat) for city in _RNG.choices(_ALL_CITIES, k=DATASET_SIZE)
+]
 
 
-def _test_tzfpy_fixed_dataset_batch():
-    for lng, lat in _FIXED_POINTS:
+class QueryStream:
+    def __init__(self, points):
+        self.points = points
+        self.idx = 0
+
+    def reset(self):
+        self.idx = 0
+
+    def __call__(self):
+        lng, lat = self.points[self.idx]
+        self.idx += 1
+        if self.idx == len(self.points):
+            self.idx = 0
         _ = get_tz(lng, lat)
 
 
+_QUERY_STREAM = QueryStream(_RANDOM_POINTS)
+
+# warmup lazy init
+_ = get_tz(116.3883, 39.9289)
+
+
+def _test_tzfpy_random_stream():
+    _QUERY_STREAM()
+
+
 @mark.benchmark
-def test_tzfpy_fixed_cities(benchmark):
+def test_tzfpy(benchmark):
+    _QUERY_STREAM.reset()
     benchmark.extra_info["exp_index"] = EXP_INDEX or "none"
     benchmark.extra_info["dataset_seed"] = DATASET_SEED
-    benchmark.extra_info["dataset_size"] = len(_FIXED_POINTS)
-    benchmark(_test_tzfpy_fixed_dataset_batch)
+    benchmark.extra_info["dataset_size"] = len(_RANDOM_POINTS)
+    benchmark(_test_tzfpy_random_stream)
